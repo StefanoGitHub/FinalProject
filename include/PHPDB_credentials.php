2858a185783476dd51311a41bedfa36d358c1d64
StefanoGitHub <stefano.borghi@gmail.com>
2015-06-17 16:03:50 -0700

    deleted

--- include/PHPDB_credentials.php
+++
@@ -1,8 +0,0 @@
-<?php
-//PHPDB_credentials.php
-
-define('DB_NAME',"a5316511_PHPDB");
-define('DB_USER',"a5316511_PHPDB");
-define('DB_PASSWORD',"passw0rD");
-define('DB_HOST',"mysql8.000webhost.com");
-?>
